f7b04c333118014f257d696437e1bf51460ef0e8
muhamadijlal <haidarijlal027@gmail.com>
2023-08-28 20:59:33 +0700

    init commit

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import { createApp } from "vue";
+import "@/style.css";
+import App from "@/App.vue";
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+
+library.add(fas, far);
+
+const app = createApp(App);
+app.component("font-awesome-icon", FontAwesomeIcon);
+app.mount("#app");
